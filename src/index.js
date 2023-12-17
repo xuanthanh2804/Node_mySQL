2fa81a646116198b8e38b0828490a1e532915b5f
xuanthanh2804 <natsusenpai2k4@gmail.com>
2023-12-17 13:51:39 +0700

    project init

--- src/index.js
+++ src/index.js
@@ -6,9 +6,27 @@ const route = require("./routes");
 const app = express();
 const port = process.env.PORT || 8081;
 
+//Connect DB
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  database: "testdb",
+  password: "123456",
+});
+
+connection.query("SELECT * FROM members", function (err, results, fields) {
+  console.log(">>> result=", results); // results contains rows returned by server
+  console.log(">>> fields=", fields); // fields contains extra meta data about results, if available
+});
+
 //post
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+//static file
+app.use(express.static(path.join(__dirname, "public")));
 
 // Template engine
 app.engine(
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const SiteController = require("../app/controllers/siteController");
+const SiteController = require("../controllers/siteController");
 router.get("/", SiteController.index);
 module.exports = router;
